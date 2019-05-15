8bcdabe5229c2a7a230781c284301a74fa93bd26
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-05-15 12:32:35 +0200

    sqlite connect

--- src/NewMain.java
+++ src/NewMain.java
@@ -5,7 +5,9 @@ package src;
 public class NewMain {
     public static void main(String[] args) {
         
-
+        SessionDB db = new SessionDB();
+        db.connect();
+        db.close();
 
     }
 }
